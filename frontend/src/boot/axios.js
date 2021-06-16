e0bceb83dd5faccfc6f6f44e9465e2cb60ff10fc
Praditya Raudi <pradityaraudi@gmail.com>
2021-06-16 15:59:31 -0400

    fix axios "

--- frontend/src/boot/axios.js
+++ frontend/src/boot/axios.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import axios from 'axios'
 
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Content-Type': 'application/json'
+}
+
 export const http = axios.create({
-  baseURL: process.env.NEWS_API
+  baseURL: process.env.NEWS_API,
+  headers
 })
 
 Vue.prototype.$http = http
